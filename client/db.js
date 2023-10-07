944a1e4c3f5bd813c7e326e1b61b11d779e538d9
L'victor Taylor <laurice@Lvictors-MacBook-Pro-2.local>
2023-10-07 09:53:41 -0400

    space

--- client/db.js
+++ client/db.js
@@ -10,7 +10,7 @@ const Pool = require("pg").Pool
 
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   })
 
 
