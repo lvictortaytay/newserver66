736c91991f127c8149c5e60e6f09317e75fa1570
L'victor Taylor <laurice@Lvictors-MacBook-Pro-2.local>
2023-10-07 13:13:30 -0400

    vercel

---
+++ server/db.js
@@ -0,0 +1,22 @@
+const Pool = require("pg").Pool
+
+// const pool = new Pool({
+//     "user" :"laurice",
+//     "password" : "Lvict157050",
+//     "host":"localhost",
+//     "port":5432,
+//     "database":"perntodo"
+// })
+
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
+
+
+// pool.connect((err) => {
+//     if(err) throw err
+//     console.log("connected to POSTGRESQL SUCCESSFULLY")
+// })
+
+module.exports = pool;
