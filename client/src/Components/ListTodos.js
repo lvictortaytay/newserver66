b3ed1a7966111427dc1c0383d1b87f5e3e95ec10
L'victor Taylor <laurice@Lvictors-MacBook-Pro-2.local>
2023-10-07 22:48:02 -0400

    new server6

--- client/src/Components/ListTodos.js
+++ client/src/Components/ListTodos.js
@@ -24,7 +24,7 @@ const ListTodos = () => {
     const getTodos = async () => {
         try {
             
-            const response = await fetch("https://newserver5-oy7l5dhqe-lvictors-projects.vercel.app" , {
+            const response = await fetch("https://newserver5-oy7l5dhqe-lvictors-projects.vercel.app/todos" , {
                 "mode" :"no-cors"
             })
             const jsonData = await response.json()
