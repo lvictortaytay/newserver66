bed578fb25bb81280c00e9477126fdb682b66a0d
lvictor taylor <lvictor.taylor@gmail.com>
2023-10-09 23:24:17 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.post("/todos" , async (req , res) => {
     try{
         const {description} = req.body
-        const newTodo = await pool.query('INSERT INTO todo (description) VALUES("hello") RETURNING *;')
+        const newTodo = await pool.query('INSERT INTO todo (description) VALUES("hello")')
         res.json(newTodo)
     }catch(err){
         console.error(err.message)
